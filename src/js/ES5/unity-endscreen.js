109a25dbdfb6d6acf05df63e09c2bb27d6854dd9
Amy Morris-Clark <ajc100@hotmail.com>
2017-01-02 18:12:18 -0500

    Added fail, changed button code
    - Added fail response
    - Updated $.get request syntax
    - Changed button to load through JS instead of being directly in the
    html which caused it to show before the other elements.

--- src/js/ES5/unity-endscreen.js
+++ src/js/ES5/unity-endscreen.js
@@ -1,25 +1,50 @@
 'use strict';
 
-$(function getGameInfo() {
-  $.get({
-    url: 'https://itunes.apple.com/lookup?id=1145275343',
-    dataType: 'jsonp',
-    success: function grabJSON(parsedJson) {
-      var gameName = parsedJson.results[0].trackName;
-      var gameIcon = parsedJson.results[0].artworkUrl60;
-      $('.title').html('<img src="' + gameIcon + '">' + gameName);
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-      var ObjLength = parsedJson.results[0].screenshotUrls.length;
-      for (var i = 0; i < ObjLength; i += 1) {
-        var currentImage = parsedJson.results[0].screenshotUrls[i];
-        $('.main-carousel').append('<div class="carousel-cell"><img src=\'' + currentImage + '\'></div>');
-      }
-
-      $('.main-carousel').flickity({
-        wrapAround: true,
-        freeScroll: true,
-        autoPlay: true
+(function () {
+  var getGameInfo = function getGameInfo() {
+    return new Promise(function (resolve) {
+      $.get('https://itunes.apple.com/lookup?id=1145275343', function (parsedJson) {
+        var gameName = parsedJson.results[0].trackName;
+        var gameIcon = parsedJson.results[0].artworkUrl60;
+        var ObjLength = parsedJson.results[0].screenshotUrls.length;
+        var gameImages = [];
+        for (var i = 0; i < ObjLength; i += 1) {
+          gameImages.push(parsedJson.results[0].screenshotUrls[i]);
+        }
+        resolve([gameName, gameIcon, gameImages]);
+      }, 'jsonp').fail(function () {
+        $('body').prepend('<p class="fail">Request Failed :(</p>');
       });
+    });
+  };
+
+  var build = function build(gameName, gameIcon, gameImages) {
+    $('.title').html('<img src="' + gameIcon + '">' + gameName);
+    for (var i = 0; i < gameImages.length; i += 1) {
+      var currentImage = gameImages[i];
+      $('.main-carousel').append('<div class="carousel-cell"><img src=\'' + currentImage + '\'></div>');
     }
-  });
-});
+    $('footer').html('<a href="http://apple.co/SUPERMARIORUN" target="_blank"><img alt="Download Now!" src="src/img/btn01.png"></a>');
+    $('.main-carousel').flickity({
+      wrapAround: true,
+      freeScroll: true,
+      autoPlay: true
+    });
+  };
+
+  var init = function init() {
+    getGameInfo().then(function (gameInfoArray) {
+      var _gameInfoArray = _slicedToArray(gameInfoArray, 3);
+
+      var gameName = _gameInfoArray[0];
+      var gameIcon = _gameInfoArray[1];
+      var gameImages = _gameInfoArray[2];
+
+      build(gameName, gameIcon, gameImages);
+    });
+  };
+
+  init();
+})();
--- src/js/ES6/unity-endscreen.js
+++ src/js/ES6/unity-endscreen.js
@@ -1,24 +1,46 @@
-$(function getGameInfo() {
-  $.get({
-    url: 'https://itunes.apple.com/lookup?id=1145275343',
-    dataType: 'jsonp',
-    success: function grabJSON(parsedJson) {
-      const gameName = parsedJson.results[0].trackName;
-      const gameIcon = parsedJson.results[0].artworkUrl60;
-      $('.title').html(`<img src="${gameIcon}">${gameName}`);
-
-      const ObjLength = parsedJson.results[0].screenshotUrls.length;
-      for (let i = 0; i < ObjLength; i += 1) {
-        const currentImage = parsedJson.results[0].screenshotUrls[i];
-        $('.main-carousel').append(`<div class="carousel-cell"><img src='${currentImage}'></div>`);
-      }
-
-      $('.main-carousel').flickity({
-        wrapAround: true,
-        freeScroll: true,
-        autoPlay: true,
+(function () {
+  const getGameInfo = () => {
+    return new Promise((resolve) => {
+      $.get('https://itunes.apple.com/lookup?id=1145275343',
+        (parsedJson) => {
+          const gameName = parsedJson.results[0].trackName;
+          const gameIcon = parsedJson.results[0].artworkUrl60;
+          const ObjLength = parsedJson.results[0].screenshotUrls.length;
+          const gameImages = [];
+          for (let i = 0; i < ObjLength; i += 1) {
+            gameImages.push(parsedJson.results[0].screenshotUrls[i]);
+          }
+          resolve([gameName, gameIcon, gameImages]);
+        },
+      'jsonp')
+      .fail(() => {
+        $('body').prepend('<p class="fail">Request Failed :(</p>');
       });
-    },
+    });
+  };
+
+  const build = (gameName, gameIcon, gameImages) => {
+  $('.title').html(`<img src="${gameIcon}">${gameName}`);
+    for (let i = 0; i < gameImages.length; i += 1) {
+      const currentImage = gameImages[i];
+      $('.main-carousel').append(`<div class="carousel-cell"><img src='${currentImage}'></div>`);
+    }
+  $('footer').html('<a href="http://apple.co/SUPERMARIORUN" target="_blank"><img alt="Download Now!" src="src/img/btn01.png"></a>');
+  $('.main-carousel').flickity({
+    wrapAround: true,
+    freeScroll: true,
+    autoPlay: true,
   });
-});
+  };
+
+  const init = () => {
+    getGameInfo().then((gameInfoArray) => {
+      const [gameName, gameIcon, gameImages] = gameInfoArray;
+      build(gameName, gameIcon, gameImages);
+    });
+  };
+
+  init();
+
+}());
 
